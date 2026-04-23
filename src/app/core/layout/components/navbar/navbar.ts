5344404787256d8cb60046a038a63a65d00ffabd
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-23 12:58:36 +0200

    Hamburger Icon Added

--- src/app/core/layout/components/navbar/navbar.ts
+++ src/app/core/layout/components/navbar/navbar.ts
@@ -8,4 +8,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navbar {
+  onMenuIconClick(event: Event){
+    const icon = event.target as HTMLLIElement;
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-xmark');
+  }
 }
