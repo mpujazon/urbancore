f392a435a350ce43f24ae995671113016e175d27
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-23 11:10:16 +0200

    _variables.scss added

--- src/app/core/layout/components/navbar/navbar.ts
+++ src/app/core/layout/components/navbar/navbar.ts
@@ -1,13 +1,11 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navbar {
-
 }
