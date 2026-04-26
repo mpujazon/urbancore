bb66823305b61818618cb987bbc56e3a4b7f1e73
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-26 12:47:28 +0200

    AuthService Created

---
+++ src/app/core/services/auth-service.ts
@@ -0,0 +1,40 @@
+import { inject, Injectable } from '@angular/core';
+import { Auth, authState, getIdToken, GoogleAuthProvider, signInWithPopup, signOut, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private auth: Auth = inject(Auth);
+
+  public user$: Observable<User | null> = authState(this.auth);
+
+  async loginWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+
+    try{
+      await signInWithPopup(this.auth, provider);
+    }catch(error){
+      console.error('Error signing in with Google', error);
+      throw error;
+    }
+  }
+
+  async logout(): Promise<void> {
+    try{
+      await signOut(this.auth);
+    }catch(error){
+      console.error('Error signing out', error);
+      throw error;
+    }
+  }
+
+  async getToken(): Promise<string | null> {
+    const currentUser = this.auth.currentUser;
+    if(currentUser){
+      return await getIdToken(currentUser);
+    }
+    return null;
+  }
+}
