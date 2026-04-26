a7c5286559b9b7bf042b4604e032d447eaea425f
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-26 12:54:37 +0200

    SignIn functions added and linked to sign in with google buttons

--- src/app/core/layout/components/navbar/navbar/navbar.ts
+++ src/app/core/layout/components/navbar/navbar/navbar.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { Link } from '../../../../../shared/models/LinkInterface';
 import { RouterLink } from "@angular/router";
 import { MobileUserAvatar } from "../mobile-user-avatar/mobile-user-avatar";
 import { NavLink } from "../nav-link/nav-link";
 import { NAV_LINKS } from '../../../config/nav-links';
+import { AuthService } from '../../../../services/auth-service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar',
@@ -13,15 +15,27 @@ import { NAV_LINKS } from '../../../config/nav-links';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navbar {
+  private auth = inject(AuthService);
+  user = toSignal(this.auth.user$, {initialValue: null});
+
   // TODO: links will be given by a computed expression based on a signal of AuthService.
   links: Link[] = NAV_LINKS.filter(link => link.roles.includes('unlogged'));
   isMenuOpen = signal(false);
-  isUserLogged = signal(true);
+  isUserLogged = computed(()=> this.user());
 
   onOpenMenuClick(){
     this.isMenuOpen.set(true);
   }
   onCloseMenuClick(){
     this.isMenuOpen.set(false);
+  }
+
+  onSignInWithGoogle(){
+    try{
+      this.auth.loginWithGoogle();
+    }catch(error){
+      console.error('Error');
+
+    }
   }
 }
