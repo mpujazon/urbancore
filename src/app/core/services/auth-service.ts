282f2c5daf8beb2b89a24a1e2c86c8695d2b16b5
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-28 11:56:30 +0200

    UserDto Interface created, dbUser$ and dbUser signal created, it takes the user from db and its role

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,16 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Auth, authState, getIdToken, GoogleAuthProvider, signInWithPopup, signOut, User } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, catchError, of } from 'rxjs';
+import { UserDto } from '../../shared/models/UserInterface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private auth: Auth = inject(Auth);
+  private http: HttpClient = inject(HttpClient);
 
   public user$: Observable<User | null> = authState(this.auth);
   public user = toSignal(this.user$, {initialValue: null});
+
+  public dbUser$: Observable<UserDto | null> = this.user$.pipe(
+    switchMap((firebaseUser)=>{
+    if(firebaseUser){
+      return this.http.post<UserDto>(`${environment.API_BASE_URL}/auth/sync`, {}).pipe(
+        catchError((err)=>{
+          console.error('Error syncing user with backend', err);
+          return of(null);
+        })
+      );
+    }
+    return of(null);
+  }));
+  public dbUser = toSignal(this.dbUser$, {initialValue: null});
 
   async loginWithGoogle(): Promise<void> {
     const provider = new GoogleAuthProvider();
---
+++ src/app/shared/models/UserInterface.ts
@@ -0,0 +1,6 @@
+export interface UserDto{
+  id: number;
+  firebaseUid: string;
+  email: string;
+  role: string;
+}
