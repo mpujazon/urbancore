3d947b89ea56440535e1eada18e828acb7d3bc4d
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-28 22:33:48 +0200

    Dashboard and Manage Incidents void components created, and routed in app.routes.ts

---
+++ src/app/features/admin-incidents/pages/manage-incidents/manage-incidents.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-incidents',
+  imports: [],
+  templateUrl: './manage-incidents.html',
+  styleUrl: './manage-incidents.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ManageIncidents {
+
+}
