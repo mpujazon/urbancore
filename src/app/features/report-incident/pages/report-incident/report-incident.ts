ca679322ba520060a2794bfc381eb7e0b7b18e30
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-29 13:33:11 +0200

    Added ReportIncident Page component, just structure and styles, needs refactorization and smaller components

--- src/app/features/report-incident/pages/report-incident/report-incident.ts
+++ src/app/features/report-incident/pages/report-incident/report-incident.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-report-incident',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './report-incident.html',
   styleUrl: './report-incident.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReportIncident {
-
 }
