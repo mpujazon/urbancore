eb3bc1179a30b5017ab8e21faa55794eb25e2d78
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-30 10:48:57 +0200

    ReportIncidentHeader component created

--- src/app/features/report-incident/pages/report-incident-page/report-incident-page.ts
+++ src/app/features/report-incident/pages/report-incident-page/report-incident-page.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { ReportIncidentHeader } from '../../components/report-incident-header/report-incident-header';
 
 @Component({
   selector: 'app-report-incident-page',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ReportIncidentHeader],
   templateUrl: './report-incident-page.html',
   styleUrl: './report-incident-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
