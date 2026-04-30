8aa0f77dc33b83b474b93aee42941b4de164c3a5
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-30 10:59:52 +0200

    ReportIncidentLocation component created

--- src/app/features/report-incident/components/report-incident-wizard/report-incident-wizard.ts
+++ src/app/features/report-incident/components/report-incident-wizard/report-incident-wizard.ts
@@ -1,11 +1,11 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReportIncidentForm } from '../report-incident-form/report-incident-form';
+import { ReportIncidentLocation } from '../report-incident-location/report-incident-location';
 import { ReportIncidentMedia } from '../report-incident-media/report-incident-media';
 
 @Component({
   selector: 'app-report-incident-wizard',
-  imports: [NgOptimizedImage, ReportIncidentForm, ReportIncidentMedia],
+  imports: [ReportIncidentForm, ReportIncidentLocation, ReportIncidentMedia],
   templateUrl: './report-incident-wizard.html',
   styleUrl: './report-incident-wizard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
