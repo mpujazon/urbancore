d70f77360ad79f979a40e65b26a5ac6e5ba825f2
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-01 21:31:57 +0200

    Updated the api url in auth interceptor, now uses environment variable

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -2,10 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth-service';
 import { from, switchMap } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const isUrbancoreApi = req.url.startsWith('http://localhost:8080/api') || req.url.startsWith('https://urbancore-api.onrender.com');
+  const isUrbancoreApi = req.url.startsWith('http://localhost:8080/api') || req.url.startsWith(environment.API_BASE_URL);
 
   if (!isUrbancoreApi) {
     return next(req);
