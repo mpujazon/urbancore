bbd1d069ba73c2718d34bdd0075d4790692e9f65
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-05 23:46:03 +0200

    IncidentSkeleton Component Created

---
+++ src/app/features/citizen-dashboard/components/incident-skeleton/incident-skeleton.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-skeleton',
+  imports: [],
+  templateUrl: './incident-skeleton.html',
+  styleUrl: './incident-skeleton.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IncidentSkeleton {
+
+}
