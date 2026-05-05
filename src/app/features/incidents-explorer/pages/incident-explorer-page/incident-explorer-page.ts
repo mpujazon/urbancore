5c1461489fe04fb61e19ca00f9b5caed360192aa
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-05 14:43:47 +0200

    IncidentExplorerPage Component has been cleaned and now uses IncidentCard Component

--- src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
+++ src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
@@ -1,14 +1,16 @@
-import { NgOptimizedImage } from '@angular/common';
-import {ChangeDetectionStrategy, Component, computed, inject, signal} from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSliders } from '@fortawesome/free-solid-svg-icons';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {IncidentService} from '../../../report-incident/services/incident-service';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {IncidentDto} from '../../../../shared/models/IncidentInterface';
+import {IncidentCard} from '../../../../shared/components/incident-card/incident-card';
+import {map, Observable} from 'rxjs';
+import type {IncidentCardVm, IncidentDto} from '../../../../shared/models/IncidentInterface';
+import {mapIncidentToCard} from '../../../../shared/mappers/incident.mapper';
 
 @Component({
   selector: 'app-incident-explorer-page',
-  imports: [NgOptimizedImage, FontAwesomeModule],
+  imports: [
+    IncidentCard
+  ],
   templateUrl: './incident-explorer-page.html',
   styleUrl: './incident-explorer-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +18,12 @@ import {IncidentDto} from '../../../../shared/models/IncidentInterface';
 export class IncidentExplorerPage {
   private readonly incidentService = inject(IncidentService);
 
-  protected readonly incidents = toSignal(this.incidentService.getAllIncidents(), { initialValue: null });
+  private readonly myIncidents$: Observable<IncidentCardVm[]> = this.incidentService
+    .getAllIncidents()
+    .pipe(map((incidents: IncidentDto[]) => incidents.map((incident: IncidentDto) => mapIncidentToCard(incident))));
+
+  private readonly incidentsResponse = toSignal(this.myIncidents$, { initialValue: [] as IncidentCardVm[] });
+  protected readonly incidents = computed((): IncidentCardVm[] => this.incidentsResponse() ?? []);
+
 
 }
