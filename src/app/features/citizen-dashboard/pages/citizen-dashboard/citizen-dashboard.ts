59f5c97f6a672995a01f0526f5a365e125cb2541
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-05 23:47:21 +0200

    Added loading, error and empty states in CitizenDashboard component

--- src/app/features/citizen-dashboard/pages/citizen-dashboard/citizen-dashboard.ts
+++ src/app/features/citizen-dashboard/pages/citizen-dashboard/citizen-dashboard.ts
@@ -2,11 +2,12 @@ import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core'
 import { RouterLink } from '@angular/router';
 import { IncidentCard } from '../../../../shared/components/incident-card/incident-card';
 import {CitizenDashboardStore, DashboardFilter} from '../../store/citizen-dashboard.store';
+import {IncidentSkeleton} from '../../components/incident-skeleton/incident-skeleton';
 
 
 @Component({
   selector: 'app-citizen-dashboard',
-  imports: [RouterLink, IncidentCard],
+  imports: [RouterLink, IncidentCard, IncidentSkeleton],
   templateUrl: './citizen-dashboard.html',
   styleUrl: './citizen-dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,6 +15,7 @@ import {CitizenDashboardStore, DashboardFilter} from '../../store/citizen-dashbo
 })
 export class CitizenDashboard implements OnInit{
   protected readonly store = inject(CitizenDashboardStore);
+  protected readonly skeletonItems = Array.from({ length: 6 });
 
   ngOnInit(): void {
     this.store.loadIncidents();
