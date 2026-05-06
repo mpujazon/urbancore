0ff2f599386b47c4ebe1e1be0d83e3a07cad03fe
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-06 16:02:28 +0200

    IncidentExplorerPage is rendering the map from IncidentsExplorerMapFacade

--- src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
+++ src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
@@ -1,10 +1,23 @@
-import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  ElementRef,
+  inject,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {IncidentService} from '../../../report-incident/services/incident-service';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {IncidentCard} from '../../../../shared/components/incident-card/incident-card';
 import {map, Observable} from 'rxjs';
 import type {IncidentCardVm, IncidentDto} from '../../../../shared/models/IncidentInterface';
 import {mapIncidentToCard} from '../../../../shared/mappers/incident.mapper';
+import {IncidentsExplorerMapFacade} from '../../services/incidents-explorer-map.facade';
+import {LeafletMapService} from '../../../../shared/services/leaflet-map-service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-incident-explorer-page',
@@ -14,9 +27,46 @@ import {mapIncidentToCard} from '../../../../shared/mappers/incident.mapper';
   templateUrl: './incident-explorer-page.html',
   styleUrl: './incident-explorer-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ IncidentsExplorerMapFacade ]
 })
-export class IncidentExplorerPage {
+export class IncidentExplorerPage implements AfterViewInit, OnDestroy{
   private readonly incidentService = inject(IncidentService);
+  private readonly leafletMapService = inject(LeafletMapService);
+  private readonly mapFacade = inject(IncidentsExplorerMapFacade);
+
+  @ViewChild('mapContainer', { static: true })
+  private mapContainer?: ElementRef<HTMLElement>;
+
+  private readonly defaultCenter: L.LatLngTuple = [41.3874, 2.1686];
+  private readonly defaultZoom = 13;
+
+  ngAfterViewInit(): void {
+    if(!this.mapContainer){
+      return;
+    }
+
+    this.mapFacade.setMap(this.leafletMapService.createMap(
+      this.mapContainer.nativeElement,
+      this.defaultCenter,
+      this.defaultZoom
+    ));
+
+    requestAnimationFrame(() => {
+      this.mapFacade.map()?.invalidateSize();
+    });
+
+    setTimeout(() => {
+      this.mapFacade.map()?.invalidateSize();
+    }, 200);
+  }
+
+  ngOnDestroy(): void{
+    if(!this.mapFacade.map()){
+      return;
+    }
+
+    this.mapFacade.destroy();
+  }
 
   private readonly myIncidents$: Observable<IncidentCardVm[]> = this.incidentService
     .getAllIncidents()
@@ -24,6 +74,10 @@ export class IncidentExplorerPage {
 
   private readonly incidentsResponse = toSignal(this.myIncidents$, { initialValue: [] as IncidentCardVm[] });
   protected readonly incidents = computed((): IncidentCardVm[] => this.incidentsResponse() ?? []);
+
+
+
+
 
 
 }
