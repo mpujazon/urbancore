d0c4bc186c184bf7cd86d117a65aef63b8c399ce
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-06 16:32:45 +0200

    IncidentExplorerPage map is now usable on mobile and tablet

--- src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
+++ src/app/features/incidents-explorer/pages/incident-explorer-page/incident-explorer-page.ts
@@ -4,9 +4,10 @@ import {
   Component,
   computed,
   ElementRef,
+  HostListener,
   inject,
   OnDestroy,
-  OnInit,
+  signal,
   ViewChild
 } from '@angular/core';
 import {IncidentService} from '../../../report-incident/services/incident-service';
@@ -15,7 +16,7 @@ import {IncidentCard} from '../../../../shared/components/incident-card/incident
 import {map, Observable} from 'rxjs';
 import type {IncidentCardVm, IncidentDto} from '../../../../shared/models/IncidentInterface';
 import {mapIncidentToCard} from '../../../../shared/mappers/incident.mapper';
-import {IncidentsExplorerMapFacade} from '../../services/incidents-explorer-map.facade';
+import {IncidentExplorerMapFacade} from '../../services/incident-explorer-map-facade';
 import {LeafletMapService} from '../../../../shared/services/leaflet-map-service';
 import * as L from 'leaflet';
 
@@ -27,18 +28,20 @@ import * as L from 'leaflet';
   templateUrl: './incident-explorer-page.html',
   styleUrl: './incident-explorer-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [ IncidentsExplorerMapFacade ]
+  providers: [ IncidentExplorerMapFacade ]
 })
 export class IncidentExplorerPage implements AfterViewInit, OnDestroy{
   private readonly incidentService = inject(IncidentService);
   private readonly leafletMapService = inject(LeafletMapService);
-  private readonly mapFacade = inject(IncidentsExplorerMapFacade);
+  private readonly mapFacade = inject(IncidentExplorerMapFacade);
 
   @ViewChild('mapContainer', { static: true })
   private mapContainer?: ElementRef<HTMLElement>;
 
   private readonly defaultCenter: L.LatLngTuple = [41.3874, 2.1686];
   private readonly defaultZoom = 13;
+
+  protected readonly isMobileMapOpen = signal(false);
 
   ngAfterViewInit(): void {
     if(!this.mapContainer){
@@ -66,6 +69,30 @@ export class IncidentExplorerPage implements AfterViewInit, OnDestroy{
     }
 
     this.mapFacade.destroy();
+  }
+
+  protected openMobileMap(): void {
+    this.isMobileMapOpen.set(true);
+
+    requestAnimationFrame(() => {
+      this.mapFacade.map()?.invalidateSize();
+    });
+
+    setTimeout(() => {
+      this.mapFacade.map()?.invalidateSize();
+    }, 200);
+  }
+
+  protected closeMobileMap(): void {
+    this.isMobileMapOpen.set(false);
+  }
+
+  @HostListener('window:resize')
+  protected onViewportResize(): void {
+    if (window.innerWidth >= 992 && this.isMobileMapOpen()) {
+      this.isMobileMapOpen.set(false);
+      this.mapFacade.map()?.invalidateSize();
+    }
   }
 
   private readonly myIncidents$: Observable<IncidentCardVm[]> = this.incidentService
